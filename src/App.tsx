67db55971f7722029e1aa03c4a9107dbe7a30ae3
Vinicius Almeida Issa <vinicius.almeida@rockcontent.com>
2022-05-28 11:38:48 -0300

    chore: config eslint

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="teste">
+      <h1>My React from Scratch css</h1>
+    </div>
+  );
+}
